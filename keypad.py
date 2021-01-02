24515a984b8d6603076ce1124fc67b6522a4f4a0
roland <roland@textnibble.com>
2021-01-01 21:08:20 -0600

    got some events working between the widgets and my custom pin number widget

--- keypad.py
+++ keypad.py
@@ -1,5 +1,13 @@
+from kivy.properties import BooleanProperty
 from kivy.uix.gridlayout import GridLayout
 
 
 class Keypad(GridLayout):
-    pass
+    disabled = BooleanProperty(True)
+
+    def __init__(self, **kwargs):
+        super(Keypad, self).__init__(**kwargs)
+        self.register_event_type('on_key_pressed')
+
+    def on_key_pressed(self, val):
+        pass
--- pinnumber.py
+++ pinnumber.py
@@ -1,4 +1,39 @@
-class PinNumber:
-    def __init__(self, size: int = 4):
-        super(self).__init__(size)
+from kivy.properties import BooleanProperty
+from kivy.uix.widget import Widget
+from typing import List
 
+
+class PinNumber(Widget):
+    is_complete = BooleanProperty(False)
+
+    def __init__(self, **kwargs):
+        super(PinNumber, self).__init__(**kwargs)
+        self.register_event_type('on_clear')
+        self.register_event_type('on_add_digit')
+        self.max_size: int = 4
+        self.pin: List[int] = []
+
+    def add_digit(self, val: int):
+
+        if not self.is_complete:
+            print('adding digi')
+            self.pin.append(val)
+        else:
+            print('NOT adding digi')
+        self.is_complete = not (len(self.pin) < self.max_size)
+        print(f"\nPin length: {len(self.pin)}")
+        print(f"Max Size: {self.max_size}")
+        print(f"is_complete: {self.is_complete}")
+        self.dispatch('on_add_digit')
+
+    def clear(self):
+        print("clearing pin")
+        self.pin.clear()
+        self.is_complete = False
+        self.dispatch('on_clear')
+
+    def on_clear(self):
+        pass
+
+    def on_add_digit(self):
+        pass
